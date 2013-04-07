a5220adc695c98436af09c07b6b80e442075701e
ladyalice <alicejoyk@gmail.com>
2013-04-07 01:16:15 -0700

    Initial Changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snowbody::Application.config.secret_token = '418821cf5ef30b4404854b49d6d8a926b4c6ae0a2fd07741224e43978467de8cc84cb5e52be047de3ba3bebf30e4016b7cdfd836aa74991ad3b7fc5377d6e33a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Snowbody::Application.config.session_store :cookie_store, key: '_snowbody_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Snowbody::Application.config.session_store :active_record_store
