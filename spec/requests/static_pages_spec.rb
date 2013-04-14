c8a538223c482d609cd81f667ea760bd43b71852
ladyalice <alicejoyk@gmail.com>
2013-04-14 15:05:43 -0700

    Add links to Home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) {"Alices List"}
 	describe "Home page" do
     it "should have the h1 title"
       visit '/static_pages/home'
-      page.should have_selector:('h1', :text=> 'Alice\'s List')
+      page.should have_selector('h1', :text=> 'Alice\'s List')
     end
 
     it "works! " do
     	visit '/static_pages/home'
-    	page.should have_content('Alice\'s List')
+    	page.should have_selector('title', :text => "#{Alices List} | Home")
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       #get static_pages_index_path
       #response.status.should be(200)
@@ -19,9 +20,9 @@ describe "Static pages" do
 
   describe "About page" do
 
-  	it "should have the content 'About'"
+  	it "should have the content 'About'" do
   	visit '/static_pages/about'
-  	page.should have_content('About')
+  	page.should have_selector('h1', :text => 'About')
   end
 end
 	
@@ -31,7 +32,7 @@ end
 		visit '/static_pages/map'
 		page.should have_content('Map')
 	end
-end
+
 
 
 end
